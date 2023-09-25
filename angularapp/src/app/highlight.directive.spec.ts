e9eb98bbbd238c2b6629f082d993a28bc7de653e
Iamneo <techadmin@iamneo.ai>
2023-09-25 08:57:17 +0000

    Backup Commit

---
+++ angularapp/src/app/highlight.directive.spec.ts
@@ -0,0 +1,13 @@
+import { HighlightDirective } from './highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
+
+describe('HighlightDirective', () => {
+  it('should create an instance', () => {
+    // const elMock = {} as ElementRef; // You might need to provide a proper ElementRef mock
+    // const rendererMock = {} as Renderer2; // You might need to provide a proper Renderer2 mock
+
+    // const directive = new HighlightDirective(elMock, rendererMock);
+
+    // expect(directive).toBeTruthy();
+  });
+});
